2a5ad1fea4c29656652c5ceb1eece20c69ad4f9d
Maurits van Mastrigt <maurits@nerdieworks.nl>
2015-11-06 16:50:38 +0100

    Improve error handling.

--- js/connector.js
+++ js/connector.js
@@ -11,7 +11,7 @@ var port = 3001
 var concavaUrl = 'http://localhost:3000/'
 var keyrock = {
 	url: 'http://concava:5000/v3/',
-	adminToken: 'f53779e909834a01abdceca8ea4f197e',
+	adminToken: 'b0cf392a9562445d8cb222038010716a',
 }
 
 // Define method for authentication mocking
@@ -47,8 +47,9 @@ function getUserByToken (token, cb) {
 app.use(function (req, res, next) {
 	if (req.method === 'POST') return next()
 
-	res.writeHead(404)
-	res.end('Not found.')
+	var err = new Error('Not found.')
+	err.statusCode = 404
+	next(err)
 })
 
 // Parse query
@@ -61,9 +62,9 @@ app.use(function (req, res, next) {
 app.use(function (req, res, next) {
 	getUserByToken(req.query.token, function (err, user) {
 		if (err) {
-			res.writeHead(401)
-			res.end('Invalid authentication token.')
-			return
+			err = new Error('Invalid authorization token.')
+			err.statusCode = 401
+			return next(err)
 		}
 
 		req.user = user
@@ -78,8 +79,9 @@ app.use(bodyParser.text({ type: 'text/xml' }))
 app.use(function (req, res, next) {
 	if (req.body) return next()
 
-	res.writeHead(204)
-	res.end('No content.')
+	var err = new Error('No content.')
+	err.statusCode = 204
+	next(err)
 })
 
 // Parse payload hex and id from XML body
@@ -88,14 +90,14 @@ app.use(function (req, res, next) {
 	var hex = req.body.match(/<payload_hex>([a-fA-F0-9]{1,51})<\/payload_hex>/)[1]
 
 	if ( ! id) {
-		res.writeHead(400)
-		res.end('Invalid payload ID.')
-		return
+		var err = new Error('Invalid payload ID.')
+		err.statusCode = 400
+		return cb(err)
 	}
 	if ( ! hex) {
-		res.writeHead(400)
-		res.end('Invalid payload hex.')
-		return
+		var err = new Error('Invalid payload hex.')
+		err.statusCode = 400
+		return cb(err)
 	}
 
 	req.buffer = new Buffer(id + hex, 'hex')
@@ -137,11 +139,19 @@ app.use(function (req, res, next) {
 
 // Error handler
 app.use(function (err, req, res, next) {
-	console.error(err, err.stack)
-	if (err instanceof Error) err = err.toString()
-	res.writeHead(500)
-	res.end(err || '')
-	next()
+	console.error(err)
+	if (err.stack) console.error(err.stack)
+
+	var statusCode = (err.statusCode || 500)
+	res.writeHead(statusCode)
+
+	if (err instanceof Error) err = err.toString().replace(/^Error: /, '')
+
+	if (statusCode === 500) {
+		res.end('An internal server error occured.')
+	} else {
+		res.end(err || '')
+	}
 })
 
 // Start server
